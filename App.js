68a2644ec875f689058f4e361751608457c916de
jayweb007 <jayweb007@gmail.com>
2022-02-11 15:57:17 -0600

    commit on master

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Platform } from "react-native";
+import Amplify from "aws-amplify";
+import { withAuthenticator } from "aws-amplify-react-native";
+
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Navigation from "./navigation/Navigation";
 import { getStatusBarHeight } from "react-native-status-bar-height";
+import awsconfig from "./src/aws-exports";
+
+Amplify.configure(awsconfig);
 
 const statusBarHeight = getStatusBarHeight();
 const App = () => {
@@ -15,7 +21,7 @@ const App = () => {
   );
 };
 
-export default App;
+export default withAuthenticator(App);
 
 const styles = StyleSheet.create({
   container: {
